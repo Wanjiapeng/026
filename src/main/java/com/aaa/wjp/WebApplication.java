e0065af2bf45091f818b89d26fa59592d9671f30
wanjiapeng <920906125@qq.com>
2020-07-20 15:14:53 +0800

    更新了pom文件，以war打包

--- src/main/java/com/aaa/wjp/WebApplication.java
+++ src/main/java/com/aaa/wjp/WebApplication.java
@@ -3,11 +3,18 @@ package com.aaa.wjp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(WebApplication.class);
         springApplication.run(args);
     }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
 }
